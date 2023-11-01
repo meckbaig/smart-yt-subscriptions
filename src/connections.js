d0db2bc9be155ff448c11af7beda20243cd508c6
Семён Бородяев <meckbaig@yandex.ru>
2023-11-01 20:18:12 +0600

    folders selection and more bugs

--- src/connections.js
+++ src/connections.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const axiosClient = axios.create({
-  // baseURL:  "https://folderdataapi.somee.com/" 
-  baseURL:  "http://localhost:5082/" 
+  baseURL:  "https://folderdataapi.somee.com/" 
+  // baseURL:  "http://localhost:5082/" 
    });
 
 export const axiosGoogle = axios.create({
