3d6724d428aeeb0a4944823631ace31f4f7a5bbf
meckbaig <meckbaig@yandex.ru>
2023-11-28 19:13:30 +0600

    host changed 1

--- src/connections.js
+++ src/connections.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosClient = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? "https://folderdataapi.somee.com/" : "http://localhost:5082/" 
+  baseURL: process.env.NODE_ENV === 'production' ? "http://syts-backend.ru/" : "http://syts-backend.ru/" 
    });
 
 export const axiosGoogle = axios.create({
