77227b29c251705a021de6778aa5fc7a9e1d6c99
Семён Бородяев <meckbaig@yandex.ru>
2024-09-05 18:19:55 +0500

    Change for correct folder parameters in GUI

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,45 +5,46 @@
             <h3 @click="pMsg" class="text-danger ">Внимание!</h3>
             <p class="text-danger my-1">Требования для использования сервиса:</p>
             <p class="text-danger my-1">1) На аккаунт Google должен быть зарегистрирован профиль (канал) YouTube</p>
-            <p class="text-danger my-1">2) Подписки Вашего профиля YouTube должны быть открыты (открыть подписки можно 
-               <a href="https://www.youtube.com/account_privacy" class="text-danger" target="_blank">здесь</a>)</p>
+            <p class="text-danger my-1">2) Подписки Вашего профиля YouTube должны быть открыты (открыть подписки можно
+               <a href="https://www.youtube.com/account_privacy" class="text-danger" target="_blank">здесь</a>)
+            </p>
             <p class="text-danger my-1">3) Ваш профиль должен иметь хотя-бы одну подписку</p>
             <p class="text-danger mt-2">Если все вышеперечисленные требования выполнены - проверьте,
-                правильно ли отображается <a v-bind:href="'https://www.youtube.com/channel/'+store.state.user.youtubeId"
+               правильно ли отображается <a v-bind:href="'https://www.youtube.com/channel/' + store.state.user.youtubeId"
                   class="text-danger" target="_blank">Ваш канал</a></p>
-            <p class="text-danger my-1">Если ссылка ведёт не на Ваш канал, вы можете изменить идентификатор канала в настройках</p>
-            <p class="text-danger my-1 mb-4">Идентификатор канала можно получить в разделе "О канале", нажав на кнопку "поделиться" и выбрав "Скопировать идентификатор канала"</p>
+            <p class="text-danger my-1">Если ссылка ведёт не на Ваш канал, вы можете изменить идентификатор канала в
+               настройках</p>
+            <p class="text-danger my-1 mb-4">Идентификатор канала можно получить в разделе "О канале", нажав на кнопку
+               "поделиться" и выбрав "Скопировать идентификатор канала"</p>
          </span>
          <div class="d-flex flex-row justify-content-between">
             <h3>Ваши папки:</h3>
             <button @click="createFolder" class="btn btn-secondary">+ Добавить папку</button>
          </div>
          <div class="d-flex flex-row my-2 align-self-start flex-wrap gap-2">
-            <div v-if="loadingTextF != '' && store.state.folders.length == 0" 
-               class="h3 text-center"> {{ loadingTextF }}
+            <div v-if="loadingTextF != '' && folders.length == 0" class="h3 text-center"> {{ loadingTextF }}
             </div>
-            <h3 v-else-if="store.state.folders.length == 0" class="w-100 text-center text-muted my-5">
+            <h3 v-else-if="folders.length == 0" class="w-100 text-center text-muted my-5">
                Вы не создали ни одной папки
             </h3>
-            <folder-item v-else v-for="folder in store.state.folders" :id="folder.id" :name="folder.name"
-               :color="folder.color" :icon="folder.icon" :channelsCount="folder.channelsCount" :editable="true"
-               style="max-width: 180px;" />
+            <folder-item v-else v-for="folder in folders" :guid="folder.guid" :name="folder.name" :color="folder.color"
+               :icon="folder.icon" :channelsCount="folder.channelsCount" :editable="true" style="max-width: 180px;" />
          </div>
          <hr>
       </div>
       <h3 class="mb-3">Папки в открытом доступе:</h3>
       <div class="d-flex flex-row my-2 align-self-start flex-wrap gap-2">
-         <div v-if="loadingTextPF != '' && !store.state.publicFolders.length" class="h3 text-center"> {{ loadingTextPF }}
+         <div v-if="loadingTextPF != '' && publicFolders.length == 0" class="h3 text-center"> {{ loadingTextPF }}
          </div>
-         <folder-item v-if="store.state.publicFolders.length" v-for="folder in store.state.publicFolders" :id="folder.id"
-            :name="folder.name" :color="folder.color" :icon="folder.icon" :channelsCount="folder.channelsCount"
-            :editable="false" style="max-width: 180px;" />
+         <folder-item v-if="publicFolders.length" v-for="folder in publicFolders" :guid="folder.guid" :name="folder.name"
+            :color="folder.color" :icon="folder.icon" :channelsCount="folder.channelsCount" :editable="false"
+            style="max-width: 180px;" />
       </div>
    </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import FolderItem from "../components/FolderItem.vue";
 import { useRoute } from 'vue-router';
 import router from '../router'
@@ -53,6 +54,8 @@ const warning = ref('')
 const loadingTextF = ref('')
 const loadingTextPF = ref('')
 loadingTextPF.value = "Загрузка..."
+const folders = computed(() => store.state.folders)
+const publicFolders = computed(() => store.state.publicFolders)
 const route = useRoute()
 if (route.query.f != undefined) {
    let routeName = route.query.e == true ? "editFolder" : "folder";
@@ -72,7 +75,7 @@ onMounted(() => {
       if (store.state.user.id) {
          userId = store.state.user.id
       }
-      store.dispatch("setPublicFolders", userId)
+      store.dispatch("getFolders", userId)
    }
    const unsubscribeSetChannels = store.subscribe(async (mutations, state) => {
       if (mutations.type == 'setChannels') {
@@ -88,7 +91,7 @@ onMounted(() => {
       }
    })
    const unsubscribeSetPublicFolders = store.subscribe(async (mutations, state) => {
-      if (mutations.type == 'setPublicFolders') {
+      if (mutations.type == 'getFolders') {
          loadingTextPF.value = ""
          unsubscribeSetPublicFolders()
       }
