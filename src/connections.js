cb6e4c715a1a18a6ed9b5164af79252059dc438b
Семён Бородяев <meckbaig@yandex.ru>
2024-09-16 14:14:27 +0500

    mainDomain change to function

--- src/connections.js
+++ src/connections.js
@@ -1,13 +1,15 @@
 import axios from 'axios'
 
 export const axiosClient = axios.create({
-  baseURL: mainDomain + "/api"
+  baseURL: mainDomain() + "/api"
 });
 
 export const axiosClientV1 = axios.create({
-  baseURL: mainDomain + "/api/v1"
+  baseURL: mainDomain() + "/api/v1"
 });
 
-const mainDomain = process.env.NODE_ENV === 'production' 
-    ? "https://www.smart-youtube-subscriptions-backend.ru" 
-    : "https://localhost:7150"
+function mainDomain() {
+    return process.env.NODE_ENV === 'production' 
+        ? "https://www.smart-youtube-subscriptions-backend.ru" 
+        : "https://localhost:7150";
+}
